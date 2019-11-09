420821df411748091e6cd53b3ea15c10fc26032c
root <satish.navkar@gmail.com>
2019-11-09 22:20:45 +0530

    first commit

---
+++ py-ansibleT-api.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python
+
+#
+# This is basic code just started in search of python app that can communicate with AnsibleTower
+# One must have python installed with towerlib module as well. Here pip or easy_install should help.
+# pip install towerlib --user   --->  one can try anaconda prompt and Spyder as per choice
+#
+
+from towerlib import Tower
+
+#to connect with ansible tower using valid authentication
+tower = Tower('xyz-ansibleT.com', 'Username', 'password', secure=True, ssl_verify=False)
+
+#to print list of servers in the inventory
+for host in tower.hosts:
+    print(host.name)
